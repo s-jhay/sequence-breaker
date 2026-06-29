a747332273718f98a958d478a111358a31a6236e
Scott Demmert <73268848+s-jhay@users.noreply.github.com>
2026-06-29 16:15:34 -0400

    finish program

--- sequencebreaker.py
+++ sequencebreaker.py
@@ -1,11 +1,12 @@
-# TODO: check that sequences are at least 60 chars long, change using modulo if not
-# TODO: write sequence to output of choice, working one block at a time
+import time
 
 INPUT_FILE = "input.txt"
 OUTPUT_FILE = "output.txt" # accepts .txt or .csv
 BLOCK_LENGTH = 60
 
+start_time = time.perf_counter()
 print("Starting...")
+
 sequences = {}
 seq_name = None
 seq_lines = []
@@ -15,7 +16,6 @@ with open(INPUT_FILE, "r", encoding="utf-8") as file:
 
     print("File found.")
     for line in file:
-        # print(line)
         line = line.strip()
 
         if line.startswith(">"):
@@ -35,7 +35,7 @@ with open(INPUT_FILE, "r", encoding="utf-8") as file:
     if seq_name is not None:
         sequences[seq_name] = "".join(seq_lines)
 
-    print("Sequences loaded.")
+    print(f"{len(sequences.keys())} sequences loaded.")
 
 length_map = {name: len(seq) for name, seq in sequences.items()}
 lengths = set(length_map.values())
@@ -46,7 +46,33 @@ if len(lengths) != 1:
         f"Lengths per sequence: {length_map}"
     )
 
+lengths_count = lengths.pop()
+if lengths_count < BLOCK_LENGTH:
+    print(f"Setting Block length to {lengths_count}")
+    BLOCK_LENGTH = lengths_count
+print(f"Block length set to {BLOCK_LENGTH}.")
 
+name_max_length = max(len(name) for name in sequences.keys())
+with open("output.txt", "w") as output:
+    for line_start in range(0, lengths_count, BLOCK_LENGTH):
+        line_end = min(line_start + BLOCK_LENGTH, lengths_count)
+        sequence_blocks = [sequence[line_start:line_end] for sequence in sequences.values()]
 
+        consensus_line = []
+        for column in zip(*sequence_blocks):
+            if len(set(column)) == 1:
+                consensus_line.append("*")
+            else:
+                consensus_line.append(".")
+        
+        consensus_line = "".join(consensus_line)
 
+        for name, block in zip(sequences.keys(), sequence_blocks):
+            output.write(f"{name:<{name_max_length}} {block} {line_end}\n")
+        
+        output.write(f"{'':<{name_max_length}} {consensus_line}\n\n")
+
+elapsed = time.perf_counter() - start_time
+print(f"Done! Completed in {elapsed:.3f} seconds.")
+print(f"Output written to output file: {OUTPUT_FILE}")
 
